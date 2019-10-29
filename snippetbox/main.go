409ccdaf187788239e3e8d198d38f50c73b388fd
Everton Pereira <everton.mwp@gmail.com>
2019-10-28 21:39:12 -0300

    add url query strings

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Displaying a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed!", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
-
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on http://localhost:4000")
+
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
