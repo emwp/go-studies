552380e043b54f8b61a5c1478914582315a95ef5
Everton Pereira <everton.mwp@gmail.com>
2019-10-30 22:48:29 -0300

    connecting to mysql db

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,12 +15,31 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "web:3400@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +53,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on http://localhost%s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
--- snippetbox/go.mod
+++ snippetbox/go.mod
@@ -2,4 +2,8 @@ module github.com/emwp/go-studies/snippetbox
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/sirupsen/logrus v1.4.2
+	google.golang.org/appengine v1.6.5 // indirect
+)
