5b91059d7c6b899c398dbcab610698208c4f3e7a
Everton Pereira <everton.mwp@gmail.com>
2020-01-04 23:51:55 -0300

    Add RPC call with deadline

--- grpc-go/greet/greet_server/server.go
+++ grpc-go/greet/greet_server/server.go
@@ -11,6 +11,8 @@ import (
 
 	"github.com/emwp/go-studies/grpc-go/greet/greetpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct{}
@@ -87,6 +89,26 @@ func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) er
 			log.Fatalf("Something went wrong while sending data to client: %v", err)
 		}
 	}
+}
+
+func (*server) GreetWithDeadline(ctx context.Context, req *greetpb.GreetWithDeadlineRequest) (*greetpb.GreetWithDeadlineResponse, error) {
+	fmt.Printf("\nGreetWithDeadline function was invoked with %v\n", req)
+
+	for i := 0; i < 3; i++ {
+		if ctx.Err() == context.Canceled {
+			fmt.Println("The client cancelled the request!")
+			return nil, status.Error(codes.Canceled, "The client cancelled the request!")
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	firstName := req.GetGreeting().GetFirstName()
+
+	res := &greetpb.GreetWithDeadlineResponse{
+		Result: "Hello " + firstName,
+	}
+
+	return res, nil
 }
 
 func main() {
